876fe5d815eb289391a744227d4414395b0d3504
tomdoeslinux-2 <tomdoeslinux@protonmail.com>
2025-07-29 21:03:40 +1200

    create two starting tables

---
+++ create_tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Patient (
+    NHI VARCHAR(10) PRIMARY KEY,
+    Name VARCHAR(100),
+    DOB DATE
+);
+
+CREATE TABLE Referral (
+    ReferralID INT PRIMARY KEY AUTO_INCREMENT,
+    NHI VARCHAR(10),
+    ReferralDate DATE,
+    ReferredBy VARCHAR(100),
+    Department VARCHAR(100),
+    AddedToWaitlistDate DATE,
+    Surgeon VARCHAR(100),
+    FsaDate DATE,
+    HealthTargetEligible BOOLEAN,
+    FOREIGN KEY (NHI) REFERENCES Patient(NHI)
+);
